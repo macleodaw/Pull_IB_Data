ad2e11ed80453dacca0cd9515ed1d96b7665fdf7
Allan W MacLeod <macleodaw@gmail.com>
2020-07-22 08:57:18 +0100

    Committed

--- Extract sector fundamentals.py
+++ Extract sector fundamentals.py
@@ -1,13 +1,39 @@
+import numpy as np
 import pandas as pd
-from pandas import DataFrame
+import matplotlib.pyplot as plt
 
 full_dataframe=[]
 data = pd.read_csv(r'LSE_Financials_FTSE100.csv')
-#print(data)
 
+#controls
 slice_by='Sector'
 variable_to_search=' Industrials '
+Fundamental='Dividend_per_Share'
 
+#slicing the data
 data=data[data[slice_by] == variable_to_search]
+data=data[['EPIC','Company_Name','Sector','SubSector','Financial_YE','Curreny',Fundamental]]
 
-print(data)
+#print(data)
+dps=data[[Fundamental]]
+
+x_axis=list(range(0, 50))
+y_axis=list(range(1, 85))
+
+new_list = []
+count = 0
+print(len(dps))
+
+while (count <= 10):
+    dps_ind=(dps.iloc[count,0])
+    if dps_ind == "-":
+        dps_ind=0
+    dps_ind = float(dps_ind)
+    new_list.append(dps)
+    count=count+1
+
+print(new_list)
+print(len(new_list))
+#plt.scatter(x_axis,new_list)
+#plt.show()
+
